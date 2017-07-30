d329421ba0f4a68350e5bbe05983728f377db1e9
Andrew Xing <xingandrew1@gmail.com>
2017-07-30 09:25:13 -0400

    Create CCC '07 J1 Who is in the Middle?.py

---
+++ CCC/CCC '07 J1 Who is in the Middle?.py
@@ -0,0 +1,18 @@
+#One of my earliest submissions when I was just starting to program ^^
+
+one = input()
+two = input()
+three = input()
+
+if one > two and one < three:
+  print one
+elif one > three and one < two:
+  print one
+elif two > one and two < three: 
+  print two
+elif two > three and two < one:
+  print two
+elif three > one and three < two:
+  print three
+elif three > two and three < one:
+  print three
