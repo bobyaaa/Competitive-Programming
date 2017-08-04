d8b89074ec2d54e86b67ba669eacdc6bd1119387
Andrew Xing <xingandrew1@gmail.com>
2017-08-04 00:33:07 -0400

    Create CCC '15 S3 Gates.py

---
+++ CCC '15 S3 Gates.py
@@ -0,0 +1,22 @@
+import sys
+gates = int(sys.stdin.readline())
+planes = int(sys.stdin.readline())
+
+airport = [0 for x in range(gates)]
+result = 0
+
+for x in range(planes):
+  plane = int(sys.stdin.readline()) - 1
+    
+  while plane >= 0 and airport[plane] > 0:
+    save = airport[plane]
+    airport[plane] += 1
+    plane = plane - save
+  
+  if plane < 0:
+    break
+  else:
+    airport[plane] += 1
+    result += 1
+    
+print result
