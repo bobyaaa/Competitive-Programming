0e84f2ff31122edb97d03c630feb06f87216d97d
Andrew Xing <xingandrew1@gmail.com>
2017-08-08 17:33:20 -0400

    Create  VMSS 7 WC '16 #1 Silver - Project Feng: Multiple Statements.py

---
+++ Other/VMSS 7 WC '16 #1 Silver - Project Feng: Multiple Statements.py
@@ -0,0 +1,26 @@
+#I hate this question
+#Solution by the Andrew "dumbo" Xing
+
+n = input()
+dank = []
+
+for x in range(n):
+  danker = input()
+  dank.append(danker)
+
+frequency = []
+for x in range(n):
+  if [dank[x], dank.count(dank[x])] not in frequency:
+    frequency.append([dank[x], dank.count(dank[x])])
+  
+frequency.sort()
+  
+for x in range(len(frequency)-1, -1, -1):
+  if frequency[x][0] == frequency[x][1]:
+    print frequency[x][1]
+    break
+else:
+  if 0 in dank:
+    print "Paradox!"
+  else:
+    print 0
