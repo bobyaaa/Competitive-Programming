3fd37a0b85a82871e943a15066300a2086ddd8a7
Andrew Xing <xingandrew1@gmail.com>
2017-08-10 12:54:32 -0400

    Create List Minimum.py

---
+++ Other/List Minimum.py
@@ -0,0 +1,13 @@
+#Solution by Andrew Xing
+how_many = int(input())
+
+phat_list = []
+
+for x in range(how_many):
+  new_thing = input()
+  phat_list.append(new_thing)
+  
+phat_list.sort()
+
+for y in range(len(phat_list)):
+  print phat_list[y]
