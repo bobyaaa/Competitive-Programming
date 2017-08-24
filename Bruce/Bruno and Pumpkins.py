86c17e10a9bc7959c82758b4be7eba014aff4981
Andrew Xing <xingandrew1@gmail.com>
2017-08-24 12:30:02 -0400

    Update Bruno and Pumpkins.py

--- Bruce/Bruno and Pumpkins.py
+++ Bruce/Bruno and Pumpkins.py
@@ -5,6 +5,10 @@ n = input()
 t = input()
 dp_but_not_really = [input() for x in range(n)]
 dp_but_not_really.sort()
+#We sort our dp. This is because we just want to find the optimal solutions. Say we have -4 -3 -2 1 8. Why would you try
+#to compute the distance (if you want three pumpkins) for -4, -3, 1, when clearly -4, -3, -2, will give you a better answer.
+#All the optimal answers come from indexes subsequent to one another. So like (-4, -3, -2), (-3, -2, 1), (-2, 1, 8). 
+#Those are the only things we need to check, and we can run this in O(N) time.
 
 result = 10000
 
